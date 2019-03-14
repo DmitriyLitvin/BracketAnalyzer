03170d8a0b9e1f7ba86f530e4624cf70937dfaeb
dmitriy <dimalitvin192.0@gmail.com>
2019-03-14 17:27:47 +0200

    added core style changes

--- src/test/java/AnalyzerTest.java
+++ src/test/java/AnalyzerTest.java
@@ -1,42 +1,41 @@
 import junit.framework.Assert;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class AnalyzerTest {
 
     @Test
     public void analyzeTest1() {
-        Assert.assertEquals(false,Analyzer.analyze("a(na(lyz)e"));
+        Assert.assertFalse(Analyzer.analyze("a(na(lyz)e"));
     }
 
     @Test
     public void analyzeTest2() {
-        Assert.assertEquals(true,Analyzer.analyze("an((aly))ze"));
+        Assert.assertTrue(Analyzer.analyze("an((aly))ze"));
     }
 
     @Test
     public void analyzeTest3() {
-        Assert.assertEquals(false,Analyzer.analyze("an)aly(ze"));
+        Assert.assertFalse(Analyzer.analyze("an)aly(ze"));
     }
 
     @Test
     public void analyzeTest4() {
-        Assert.assertEquals(true,Analyzer.analyze("(a(n()())al)yze"));
+        Assert.assertTrue(Analyzer.analyze("(a(n()())al)yze"));
     }
 
     @Test
     public void analyzeTest5() {
-        Assert.assertEquals(false,Analyzer.analyze("a(n()al)yz)e"));
+        Assert.assertFalse(Analyzer.analyze("a(n()al)yz)e"));
     }
 
     @Test
     public void analyzeTest6() {
-        Assert.assertEquals(true,Analyzer.analyze("a(na)l(yz)e"));
+        Assert.assertTrue(Analyzer.analyze("a(na)l(yz)e"));
     }
 
     @Test
     public void analyzeTest7() {
-        Assert.assertEquals(false,Analyzer.analyze("an)al)y(z(e"));
+        Assert.assertFalse(Analyzer.analyze("an)al)y(z(e"));
     }
 }
